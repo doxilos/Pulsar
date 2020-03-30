71c3cf541063e77b0b5a03fdab8c7c929f675542
Nurbaki Vural <nurbakivural@gmail.com>
2020-03-30 18:48:36 +0300

    fiix

--- js/index.js
+++ js/index.js
@@ -16,9 +16,9 @@ camera.position.z = 5;
 var animate = function () {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-    
+	cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
 	renderer.render(scene, camera);
 };
 
